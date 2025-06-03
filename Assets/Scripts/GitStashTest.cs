32cd85356a73406d446a16ff58852366e955978f
HeadlessCreator <silkysilver105@gmail.com>
2025-06-04 06:04:44 +0900

    StashTest done

---
+++ Assets/Scripts/GitStashTest.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class GitStashTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Stash 연습입니다");
+    }
+}
